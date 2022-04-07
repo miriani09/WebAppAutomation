62e97dd7d7f58c08f135de408ae0c6ed0ca7109b
miriani0996 <badbirds09>
2022-04-08 02:14:47 +0400

    Add project files.

---
+++ WebAppAutomation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+namespace WebAppAutomation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TheInternet internet = new TheInternet();
+            internet.startBrowser();
+            internet.openBrowser();
+            Console.WriteLine("browser started");
+
+            //Task1
+            internet.fileUpload();
+
+            //Task2
+
+
+
+            internet.closeBrowser();
+        }
+    }
+}
---
+++ WebAppAutomation/TheInternet.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAppAutomation
+{
+    internal class TheInternet
+    {
+        IWebDriver driver;
+        IWebElement ele;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new FirefoxDriver("C:/Program Files/Mozilla Firefox");
+        }
+
+        [Test]
+        public void openBrowser()
+        {
+            driver.Url = "https://the-internet.herokuapp.com/";
+        }
+        public void fileUpload()
+        {
+            try
+            {
+                driver.FindElement(By.LinkText("File Upload")).Click();
+
+                string file = "C:\\Users\\Miria\\Desktop\\file.txt";
+                driver.FindElement(By.Id("file-upload")).SendKeys(file);
+                driver.FindElement(By.Id("file-submit")).Click();
+
+                Console.WriteLine("Successfuly Uploaded!");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error");
+            }
+
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+        }
+
+    }
+}
